fbff81e967807b4c6658397e87bd1840c5e06400
James Baker <jlbaker361@gmail.com>
2025-10-14 18:41:20 -0400

    load?

---
+++ encoder.py
@@ -0,0 +1,91 @@
+import os
+import argparse
+from experiment_helpers.gpu_details import print_details
+from datasets import load_dataset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import json
+
+import torch
+import accelerate
+from accelerate import Accelerator
+from huggingface_hub.errors import HfHubHTTPError
+from accelerate import PartialState
+import time
+import torch.nn.functional as F
+from huggingface_hub import hf_hub_download
+from PIL import Image
+import random
+import wandb
+import numpy as np
+import random
+from diffusers import LCMScheduler,DiffusionPipeline,DEISMultistepScheduler,DDIMScheduler,SCMScheduler,AutoencoderDC
+from diffusers.models.attention_processor import IPAdapterAttnProcessor2_0
+from torchvision.transforms.v2 import functional as F_v2
+from torchmetrics.image.fid import FrechetInceptionDistance
+
+from transformers import AutoProcessor, CLIPModel
+try:
+    from torch.distributed.fsdp import register_fsdp_forward_method
+except ImportError:
+    print("cant import register_fsdp_forward_method")
+from diffusers.models.autoencoders.vae import DiagonalGaussianDistribution
+from huggingface_hub import create_repo,HfApi
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--mixed_precision",type=str,default="fp16")
+parser.add_argument("--project_name",type=str,default="person")
+parser.add_argument("--gradient_accumulation_steps",type=int,default=4)
+parser.add_argument("--name",type=str,default="jlbaker361/model",help="name on hf")
+parser.add_argument("--lr",type=float,default=0.0001)
+
+def main(args):
+    accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision,gradient_accumulation_steps=args.gradient_accumulation_steps)
+    print("accelerator device",accelerator.device)
+    device=accelerator.device
+    state = PartialState()
+    print(f"Rank {state.process_index} initialized successfully")
+    if accelerator.is_main_process or state.num_processes==1:
+        accelerator.print(f"main process = {state.process_index}")
+    if accelerator.is_main_process or state.num_processes==1:
+        try:
+            accelerator.init_trackers(project_name=args.project_name,config=vars(args))
+
+            api=HfApi()
+            api.create_repo(args.name,exist_ok=True)
+        except HfHubHTTPError:
+            print("hf hub error!")
+            time.sleep(random.randint(5,120))
+            accelerator.init_trackers(project_name=args.project_name,config=vars(args))
+
+            api=HfApi()
+            api.create_repo(args.name,exist_ok=True)
+
+
+    torch_dtype={
+        "no":torch.float32,
+        "fp16":torch.float16,
+        "bf16":torch.bfloat16
+    }[args.mixed_precision]
+
+    file_path = hf_hub_download(
+    repo_id="snap-research/weights2weights",
+    filename="weights_datasets/identities/all_weights.pt",   # <-- include the subfolder path here
+    repo_type="model"               # or "dataset" if it's a dataset repo
+    )
+
+
+    data_tensor=torch.load(file_path)
+
+
+if __name__=='__main__':
+    print_details()
+    start=time.time()
+    args=parser.parse_args()
+    print(args)
+    main(args)
+    end=time.time()
+    seconds=end-start
+    hours=seconds/(60*60)
+    print(f"successful generating:) time elapsed: {seconds} seconds = {hours} hours")
+    print("all done!")
