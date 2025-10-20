wrong order

diffusion.py
@@ -168,7 +168,7 @@ def main(args):
 
             with accelerator.accumulate(params):
                 
-                batch=batch["weights"].to(torch_dtype,device)
+                batch=batch["weights"].to(device,torch_dtype)
                 batch=batch.unsqueeze(1)
                 t=torch.randint(0,len(scheduler),(len(batch)),device=device).long()
                 noise=torch.randn_like(batch)