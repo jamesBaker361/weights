3abd68684ec3cf6a62e9797a0095d59b46a8ff80
James Baker <jlbaker361@gmail.com>
2025-10-23 12:42:03 -0400

    printing

--- models.py
+++ models.py
@@ -34,6 +34,7 @@ class LinearEncoder(Module):
             x=torch.nn.LeakyReLU()(x)
             _t=time_emb(t).unsqueeze(1)
             x=attention(x,_t,_t)
+            print(x)
 
 
         for layer,attention,time_emb in zip(self.up_block_list,self.up_attention_list,self.up_time_emb_list):
