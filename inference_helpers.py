0106cc092fc53db6914141bb68665417f4eeaa36
James Baker <jamesbaker@Jamess-MacBook-Air-4.local>
2025-10-31 14:45:52 -0400

    autocast

--- inference_helpers.py
+++ inference_helpers.py
@@ -1,18 +1,20 @@
 import torch
 from diffusers.schedulers.scheduling_ddim import DDIMScheduler
 from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import retrieve_timesteps
+from accelerate import Accelerator
 
 
 def infer_proj(denoiser:torch.nn.Module,
                scheduler:DDIMScheduler,
                text_prompt:str,
                dim_proj:int,
+               accelerator: Accelerator,
                device="cpu",
                dtype=torch.float32,
                num_inference_steps:int =10,
                n_samples:int=1,):
     
-    noise=torch.randn([n_samples,dim_proj],device=device,dtype=dtype)
+    noise=torch.randn([n_samples,dim_proj],device=device) #,dtype=dtype)
     
     # 4. Prepare timesteps
     timesteps, num_inference_steps = retrieve_timesteps(
@@ -21,15 +23,17 @@ def infer_proj(denoiser:torch.nn.Module,
     
     latents=noise
     
-    for i, t in enumerate(timesteps):
-        latent_model_input = scheduler.scale_model_input(latents, t)
-        t=torch.tensor([t]*n_samples).unsqueeze(-1).to(device)
-        if i==0:
-            print("t",t.size(),t.dtype,t,t.device)
-        
-        noise_pred = denoiser(latent_model_input,t)[0]
-        
-        latents = scheduler.step(noise_pred, t, latents, return_dict=False)[0]
+    with accelerator.autocast():
+    
+        for i, t in enumerate(timesteps):
+            latent_model_input = scheduler.scale_model_input(latents, t)
+            t=torch.tensor([t]*n_samples).unsqueeze(-1).to(device)
+            if i==0:
+                print("t",t.size(),t.dtype,t,t.device)
+            
+            noise_pred = denoiser(latent_model_input,t)[0]
+            
+            latents = scheduler.step(noise_pred, t, latents, return_dict=False)[0]
         
         
     return latents
