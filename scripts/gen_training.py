8cec7ff38f1ebad027a0f4c68834320c848be96e
James Baker <jamesbaker@Jamess-MacBook-Air-4.local>
2025-11-07 14:42:48 -0500

    print

--- scripts/gen_training.py
+++ scripts/gen_training.py
@@ -1,3 +1,4 @@
 for denoiser in ["linear","linear_text"]:
     command=f"sbatch -J weights --err=slurm_chip/training/{denoiser}.err --out=slurm_chip/training/{denoiser}.out "
     command+=f" runpygpu_chip.sh diffusion.py --name jlbaker361/weights-{denoiser} --denoiser {denoiser} "
+    print(command)
