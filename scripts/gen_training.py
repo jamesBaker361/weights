4fb7bf6e388cc50ac23cb973f0143dfe9b212988
James Baker <jamesbaker@Jamess-MacBook-Air-4.local>
2025-11-07 19:35:28 -0500

    grad steps

--- scripts/gen_training.py
+++ scripts/gen_training.py
@@ -1,4 +1,5 @@
 for denoiser in ["linear","linear_text"]:
     command=f"sbatch -J weights --err=slurm_chip/training/{denoiser}.err --out=slurm_chip/training/{denoiser}.out "
-    command+=f" runpygpu_chip.sh diffusion.py --name jlbaker361/weights-{denoiser} --denoiser {denoiser} --val_interval 2 --epochs 4"
+    command+=f" runpygpu_chip.sh diffusion.py --name jlbaker361/weights-{denoiser} --denoiser {denoiser} --val_interval 10 --epochs 100 "
+    command+=f" --limit -1 --gradient_accumulation_steps 8  "
     print(command)
